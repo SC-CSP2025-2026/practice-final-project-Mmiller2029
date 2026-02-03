c6654eaa22cd0d8799a65a7a79e7199d224dd3d7
Matti Miller <Matti.Miller@scsstudent.org>
2026-02-03 14:54:44 -0800

    feat: Did some javascript

--- website/js/script.js
+++ website/js/script.js
@@ -8,6 +8,44 @@ const options = {
   },
 };
 
+var d = document.getElementById("single-select");
+var value = d.options[d.selectedIndex].value;
+console.log(value);
+
+var e = document.getElementById("single-select2");
+var value2 = e.options[e.selectedIndex].value;
+console.log(value2);
+
+const loadQuotesByTags = async (value, value2) => {
+  value.preventDefault();
+  const difficulty = value.currentTarget.tags;
+  if (!difficulty) {
+    alert("difficulty not set properly");
+    return;
+  }
+
+  value2.preventDefault();
+  const category = value2.currentTarget.tags;
+  if (!category) {
+    alert("difficulty not set properly");
+    return;
+  }
+
+  const url = `https://student-api-proxy.onrender.com/api/quizmania-api.p.rapidapi.com/trivia-filtered?category=${category}&difficulty=${difficulty}`;
+
+  const options = {
+    method: "GET",
+  };
+
+  const response = await fetch(url, options);
+  const quotes = await response.json();
+  listGroup.innerHTML = "";
+  quotes.forEach((quote) => {
+    const listItem = `<li class="list-group-item">${quote.content} - ${quote.author}</li>`;
+    listGroup.insertAdjacentHTML("beforeend", listItem);
+  });
+};
+
 fadsadsadsetch(url, options)
   .then((response) =>
     response.json().then((result) => {
