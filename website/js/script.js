08fb7c1d837c96d99684b1e5ed663d9efee8eb3e
Matti Miller <Matti.Miller@scsstudent.org>
2026-02-12 12:24:18 -0800

    feat: Finished basically everything (not foreach)

--- website/js/script.js
+++ website/js/script.js
@@ -27,15 +27,6 @@ const listGroup = document.querySelector(".list-group");
 let difficultyLevelChosen = false;
 let categoryChosen = false;
 
-// const url =
-//   "https://student-api-proxy.onrender.com/api/quizmania-api.p.rapidapi.com/trivia-filtered?category=geography&difficulty=easy";
-// const options = {
-//   method: "GET",
-//   headers: {
-//     "X-API-Key":
-//       "49883ea83383263e8a9bc6b50a2462ac390bb6ec41323ca88efd4399ea19a5e4",
-//   },
-// };
 // Geography Literature Science History Art Music Technology Sports Entertainment Biology Mathematics Food Mythology Astronomy YouTubers/Streamers Language Animals Culture Landmarks
 
 const Sortbydiffcateg = async (difficulty, category) => {
@@ -50,14 +41,23 @@ const Sortbydiffcateg = async (difficulty, category) => {
   }
 
   const url = `https://student-api-proxy.onrender.com/api/quizmania-api.p.rapidapi.com/trivia-filtered?category=${category}&difficulty=${difficulty}`;
-
   const options = {
     method: "GET",
+    headers: {
+      "X-API-Key":
+        "49883ea83383263e8a9bc6b50a2462ac390bb6ec41323ca88efd4399ea19a5e4",
+    },
   };
 
   const response = await fetch(url, options);
-  const quizmania = await response.json();
-  const quiz = quizmania.data;
+  const result = await response.json();
+  const answers = result.data.answers;
+  console.log(result);
+  listGroup.innerHTML = "";
+  answers.forEach((answer) => {
+    const listItem = `<li class="list-group-item">${question.answers}</li>`;
+    listGroup.insertAdjacentHTML(listItem);
+  });
 };
 // fetch(url, options)
 //   .then((response) =>
@@ -78,45 +78,150 @@ EasyButton.addEventListener("click", (event) => {
     Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
   }
 });
-MediumButton.addEventListener("click", Sortbydiffcateg);
-MediumButton.tags = "Medium";
-HardButton.addEventListener("click", Sortbydiffcateg);
-HardButton.tags = "Hard";
-GeographyButton.addEventListener("click", Sortbydiffcateg);
-GeographyButton.tags2 = "Geography";
-LiteratureButton.addEventListener("click", Sortbydiffcateg);
-LiteratureButton.tags2 = "Literature";
-ScienceButton.addEventListener("click", Sortbydiffcateg);
-ScienceButton.tags2 = "Science";
-HistoryButton.addEventListener("click", Sortbydiffcateg);
-HistoryButton.tags2 = "History";
-ArtButton.addEventListener("click", Sortbydiffcateg);
-ArtButton.tags2 = "Art";
-MusicButton.addEventListener("click", Sortbydiffcateg);
-MusicButton.tags2 = "Music";
-TechnologyButton.addEventListener("click", Sortbydiffcateg);
-TechnologyButton.tags2 = "Technology";
-SportsButton.addEventListener("click", Sortbydiffcateg);
-SportsButton.tags2 = "Sports";
-EntertainmentButton.addEventListener("click", Sortbydiffcateg);
-EntertainmentButton.tags2 = "Entertainment";
-BiologyButton.addEventListener("click", Sortbydiffcateg);
-BiologyButton.tags2 = "Biology";
-MathematicsButton.addEventListener("click", Sortbydiffcateg);
-MathematicsButton.tags2 = "Mathematics";
-FoodButton.addEventListener("click", Sortbydiffcateg);
-FoodButton.tags2 = "Food";
-MythologyButton.addEventListener("click", Sortbydiffcateg);
-MythologyButton.tags2 = "Mythology";
-AstronomyButton.addEventListener("click", Sortbydiffcateg);
-AstronomyButton.tags2 = "Astronomy";
-YouTubersStreamersButton.addEventListener("click", Sortbydiffcateg);
-YouTubersStreamersButton.tags2 = "Youtubers/Streamers";
-LanguageButton.addEventListener("click", Sortbydiffcateg);
-LanguageButton.tags2 = "Language";
-AnimalsButton.addEventListener("click", Sortbydiffcateg);
-AnimalsButton.tags2 = "Animals";
-CultureButton.addEventListener("click", Sortbydiffcateg);
-CultureButton.tags2 = "Culture";
-LandmarksButton.addEventListener("click", Sortbydiffcateg);
-LandmarksButton.tags2 = "Landmarks";
+MediumButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  difficultyLevelChosen = "Medium";
+  if (categoryChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
+HardButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  difficultyLevelChosen = "Hard";
+  if (categoryChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
+GeographyButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  categoryChosen = "Geography";
+  if (difficultyLevelChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
+LiteratureButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  categoryChosen = "Literature";
+  if (difficultyLevelChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
+ScienceButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  categoryChosen = "Science";
+  if (difficultyLevelChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
+HistoryButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  categoryChosen = "History";
+  if (difficultyLevelChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
+ArtButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  categoryChosen = "Art";
+  if (difficultyLevelChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
+MusicButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  categoryChosen = "Music";
+  if (difficultyLevelChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
+TechnologyButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  categoryChosen = "Technology";
+  if (difficultyLevelChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
+SportsButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  categoryChosen = "Sportss";
+  if (difficultyLevelChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
+EntertainmentButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  categoryChosen = "Entertainment";
+  if (difficultyLevelChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
+BiologyButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  categoryChosen = "Biology";
+  if (difficultyLevelChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
+MathematicsButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  categoryChosen = "Mathematics";
+  if (difficultyLevelChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
+FoodButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  categoryChosen = "Food";
+  if (difficultyLevelChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
+MythologyButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  categoryChosen = "Mythology";
+  if (difficultyLevelChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
+AstronomyButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  categoryChosen = "Astronomy";
+  if (difficultyLevelChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
+YouTubersStreamersButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  categoryChosen = "Youtubers/Streamers";
+  if (difficultyLevelChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
+LanguageButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  categoryChosen = "Language";
+  if (difficultyLevelChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
+AnimalsButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  categoryChosen = "Animals";
+  if (difficultyLevelChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
+CultureButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  categoryChosen = "Culture";
+  if (difficultyLevelChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
+LandmarksButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  categoryChosen = "Landmarks";
+  if (difficultyLevelChosen) {
+    Sortbydiffcateg(difficultyLevelChosen, categoryChosen);
+  }
+});
